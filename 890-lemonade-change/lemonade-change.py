10cd9b165d19314ce12430730b686af1885dc39a
ShettyAish19 <aishwaryashetty914@gmail.com>
2025-07-20 15:11:28 +0530

    Time: 3 ms (76.38%) | Memory: 21.7 MB (94.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,30 +1,27 @@
+from typing import List
+
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        d={}
-        d[5]=d[10]=d[20]=0
-
-        for i in bills:
-            d[i]+=1
-
-            if i==10:
-                if d[5]>0:
-                    d[5]-=1
-                else:
-                    return False
-
-            elif i==20:
-                if d[10]>0 and d[5]>0:
-                    d[10]-=1
-                    d[5]-=1
-
-                elif d[5]>=3:
-                    d[5]-=3
-
-                else:
-                    return False
-
-        return True
-
-            
-
+        five = ten = 0  # Number of $5 and $10 bills we have
         
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            
+            elif bill == 10:
+                if five > 0:
+                    five -= 1
+                    ten += 1
+                else:
+                    return False  # No $5 to give change
+            
+            else:  # bill == 20
+                if ten > 0 and five > 0:  # Prefer giving 10 + 5
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:  # Otherwise, give 3 fives
+                    five -= 3
+                else:
+                    return False  # Can't give change
+        
+        return True
