7286c494751149c2a3ae6594d01417be8e997e5d
ShettyAish19 <aishwaryashetty914@gmail.com>
2025-08-13 08:46:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.01%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -6,13 +6,13 @@ class Solution:
         d={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
         def backtrack(start,path):
             if len(path)==len(digits):
-                res.append("".join(path))
+                res.append(path[:])
                 return
 
             for char in d[digits[start]]:
-                path.append(char)
-                backtrack(start+1,path)
-                path.pop()
+    
+                backtrack(start+1,path+char)
+                
 
-        backtrack(0,[])
+        backtrack(0,"")
         return res
