3088261b1b3508a02b6b940ff4be923daa833842
ShettyAish19 <aishwaryashetty914@gmail.com>
2025-08-19 10:29:49 +0530

    Time: 258 ms (10.05%) | Memory: 23.7 MB (14.33%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        d=defaultdict(int)
+        start=0
+        maxi=0
+        k=2
+        for end in range(len(fruits)):
+          if   fruits[end] not in d and k>=0:
+            d[fruits[end]]=1
+            k-=1
+          
+          elif fruits[end] in d:
+            d[fruits[end]]+=1
+
+          while k<0:
+            d[fruits[start]]-=1
+            
+            if d[fruits[start]]==0:
+              del d[fruits[start]]
+              k+=1
+            start+=1
+
+          maxi=max(maxi,sum(d.values()))
+
+        return maxi
+        
