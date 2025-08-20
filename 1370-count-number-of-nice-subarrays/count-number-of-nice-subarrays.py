60abb718bc131c9734c7603d6792d97c95a54ecb
ShettyAish19 <aishwaryashetty914@gmail.com>
2025-08-20 12:17:34 +0530

    Time: 75 ms (83.33%) | Memory: 22.3 MB (98.02%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,6 +1,6 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        n=len(nums)
+        '''n=len(nums)
         def helper(nums,k):
             j,res,s=0,0,0
 
@@ -14,6 +14,22 @@ class Solution:
 
             return res
 
-        return helper(nums,k)-helper(nums,k-1)
+        return helper(nums,k)-helper(nums,k-1)'''
+
+        for i in range(len(nums)):
+            nums[i] %= 2
+        
+        prefix_count = [0] * (len(nums) + 1)
+        prefix_count[0] = 1
+        s = 0
+        ans = 0
+        
+        for num in nums:
+            s += num
+            if s >= k:
+                ans += prefix_count[s - k]
+            prefix_count[s] += 1
+        
+        return ans
 
         
