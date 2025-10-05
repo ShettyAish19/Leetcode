06b54412db14a43b80edc0e8e2fd0e8bd713645c
ShettyAish19 <aishwaryashetty914@gmail.com>
2025-10-06 00:20:28 +0530

    Time: 14 ms (95.81%) | Memory: 19.4 MB (76.57%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        ans=[]
+        '''ans=[]
 
         for i in range(len(nums)):
             found=-1
@@ -12,6 +12,21 @@ class Solution:
 
             ans.append(found)
 
+        return ans'''
+
+        n=len(nums)
+        ans=[-1]*n
+        s=[]
+
+        for i in range(2*n):
+            num=nums[i%n]
+            while s and nums[s[-1]]<num:
+                ans[s.pop()]=num
+
+            if i<n:
+                s.append(i)
+
         return ans
 
-        
+
+
