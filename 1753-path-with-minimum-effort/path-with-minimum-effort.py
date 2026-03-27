98b54040566c7421dfa41c65c1cf8a40b6d574cc
ShettyAish19 <aishwaryashetty914@gmail.com>
2026-03-27 09:33:28 +0530

    Time: 1324 ms (9.84%) | Memory: 20.2 MB (99.64%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,7 +1,8 @@
 import heapq
+from collections import deque
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        mini=float('inf')
+        '''mini=float('inf')
         hp=[(0,0,0)] #effort,row,column
 
         r=len(heights)
@@ -22,7 +23,42 @@ class Solution:
 
                     if new_eff<effort[nx][ny]:
                         effort[nx][ny]=new_eff
-                        heapq.heappush(hp,(new_eff,nx,ny))
+                        heapq.heappush(hp,(new_eff,nx,ny))'''
+
+        rows = len(heights)
+        cols = len(heights[0])
+
+        def canreach(max_effort):
+            q = deque([(0, 0)])
+            vis = [[False]*cols for _ in range(rows)]
+            vis[0][0] = True
+
+            while q:
+                x, y = q.popleft()
+
+                if x == rows-1 and y == cols-1:
+                    return True
+
+                for dx, dy in [(-1,0),(0,-1),(1,0),(0,1)]:
+                    nx, ny = x+dx, y+dy
+
+                    if 0 <= nx < rows and 0 <= ny < cols and not vis[nx][ny]:
+                        if abs(heights[nx][ny] - heights[x][y]) <= max_effort:
+                            vis[nx][ny] = True
+                            q.append((nx, ny))
+
+            return False
+
+        left, right = 0, 10**6
+
+        while left < right:
+            mid = (left + right) // 2
+            if canreach(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
 
             
 
