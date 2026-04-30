e215623c46e51646917e5936adbd78a98ffb59aa
ShettyAish19 <aishwaryashetty914@gmail.com>
2026-04-30 11:58:39 +0530

    Time: 90 ms (87.16%) | Memory: 31.1 MB (43.45%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        def f(i,k,buy):
+        '''def f(i,k,buy):
             if i>=n or k<=0:
                 return 0
             if dp[i][k][buy] !=-1:
@@ -14,7 +14,21 @@ class Solution:
 
         n=len(prices)
         dp=[[[-1]*2 for _ in range(3)] for _ in range(n)]
-        return f(0,2,1)
+        return f(0,2,1)'''
+
+        n=len(prices)
+        buy1=float('-inf')
+        sell1=0
+        buy2=float('-inf')
+        sell2=0
+
+        for price in prices:
+            buy1=max(buy1,-price)
+            sell1=max(sell1,buy1+price)
+            buy2=max(buy2,sell1-price)
+            sell2=max(sell2,buy2+price)
+
+        return sell2
 
 
         
