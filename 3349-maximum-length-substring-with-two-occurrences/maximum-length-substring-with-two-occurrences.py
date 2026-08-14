ab784d34a2ae433b85f43ce2f17db6a818cca069
ShettyAish19 <aishwaryashetty914@gmail.com>
2026-08-14 14:19:00 +0530

    Time: 3 ms (76.16%) | Memory: 19.3 MB (60.10%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -1,6 +1,6 @@
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        maxi=0
+        '''maxi=0
         for i in range(len(s)):
             for j in range(i,len(s)):
                 d={}
@@ -15,6 +15,22 @@ class Solution:
                 if ans==True:
                     maxi=max(maxi,j-i+1)
 
+        return maxi'''
+
+
+        char_count = {}
+        left = 0
+        maxi = 0
+        
+        for right in range(len(s)):
+            char_count[s[right]] = char_count.get(s[right], 0) + 1
+            
+            while char_count[s[right]] > 2:
+                char_count[s[left]] -= 1
+                left += 1
+                
+            maxi = max(maxi, right - left + 1)
+            
         return maxi
                 
 
